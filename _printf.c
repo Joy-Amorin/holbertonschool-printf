8e192ffabf30c859c143a71d4c4e738d55ed3ebb
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2022-11-09 15:49:00 -0800

    fix c

--- _printf.c
+++ _printf.c
@@ -5,17 +5,12 @@
 
 int _printf(const char *format, ...)
 {
-
 	int h = 0;
-	
 	int j = 0;
-
-	long unsigned int i = 0;
-
+	int count = 0;
+	int i = 0;
 	char t;
-	
 	char *p;
-
 	va_list data_type;
 	
 	if (format == NULL)
@@ -23,13 +18,13 @@ int _printf(const char *format, ...)
 
 	va_start(data_type, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
-		{	
-				i++;
-				switch(format[i])
-				{	
+		{
+			i++;
+			switch(format[i])
+			{	
 					case 'd': 
 						j = va_arg(data_type, int);
 						function(j);
@@ -41,20 +36,27 @@ int _printf(const char *format, ...)
 					case 's':
 						p = va_arg(data_type, char *);
 						for (h = 0; p[h] != '\0'; h++)
+						{
 							putchar(p[h]);
+						}
 						break;
 					case 'c': 
 						t = va_arg(data_type, int);
+						if (t == '\0')
+							return (0);
 						putchar(t);
 						break;
 					case '%':
-						putchar('%');
+							putchar('%');
 						break;
 				}
 		}
 		else
+		{
 			putchar(format[i]);
+		}
 	}
+
 	va_end(data_type);
-	return (0);
+	return (count);
 }
